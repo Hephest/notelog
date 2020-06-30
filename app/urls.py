1baf1832c152751752b489e4263f5b0eb4977498
Vladyslav Asaievych <hephest1904@gmail.com>
2020-06-30 23:30:32 +0300

    Added Swagger support
    Added interactive documentation provided by `drf-yasg`
    package

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,21 @@
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
+
 from api import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Notelog API",
+        default_version='v1',
+        description="API for topics and entry manage."
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register(r'entries', views.EntryViewSet)
@@ -10,4 +24,11 @@ router.register(r'topics', views.TopicViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    url(r'^swagger/$',
+        schema_view.with_ui(
+            'swagger',
+            cache_timeout=0
+        ),
+        name='schema-swagger-ui'
+        ),
 ]
