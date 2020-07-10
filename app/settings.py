8fafed7241ab1956a1ce370e8ebd9a892c912139
Vladyslav Asaievych <hephest1904@gmail.com>
2020-07-10 23:53:09 +0300

    Added views for JWT

--- app/settings.py
+++ app/settings.py
@@ -65,6 +65,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
 }
 
 TEMPLATES = [
--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions, routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api import views
 
@@ -25,6 +26,8 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     url(r'^swagger/$',
         schema_view.with_ui(
             'swagger',
